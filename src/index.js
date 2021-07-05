2e22edc5a89881858bc5852a0f14bb9979b1a18e
ekaterina <besputova@gmail.com>
2021-07-05 18:23:24 +0300

    create food-service page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-import './sass/main.scss';
+import items from './menu.json';
+import itemTemplate from './menu-item.hbs';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const itemsList = document.querySelector('.js-menu');
+const menuItems = items.map(itemTemplate).join('');
+itemsList.innerHTML = menuItems;
+const theme = localStorage.getItem('theme');
+const themeToggle = document.querySelector('#theme-switch-toggle');
+
+if (theme === 'DARK') {
+  themeToggle.checked = true;
+  document.body.classList.add(Theme.DARK);
+} else {
+  document.body.classList.add(Theme.LIGHT);
+}
+
+themeToggle.addEventListener('change', () => {
+  const bodyElem = document.body;
+  const newTheme = bodyElem.classList.contains(Theme.LIGHT) ? 'DARK' : 'LIGHT';
+  const oldTheme = !bodyElem.classList.contains(Theme.LIGHT) ? 'DARK' : 'LIGHT';
+  bodyElem.classList.remove(Theme[oldTheme]);
+  bodyElem.classList.add(Theme[newTheme]);
+  localStorage.setItem('theme', newTheme);
+});
